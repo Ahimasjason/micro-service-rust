747bf34e8eaae06dfd02648c7594356ed7891486
Ahimasjason <ahimas.jasan@gmail.com>
2021-01-09 20:26:18 +0530

    handling GET request

--- tiny-server/src/main.rs
+++ tiny-server/src/main.rs
@@ -1,18 +1,42 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::Server;
-use hyper::{Body, Request, Response};
+use hyper::{Body, Method, Request, Response, StatusCode};
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
-async fn hello_rust(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new("Micro Service".into()))
+const INDEX: &'static str = r#"
+<!doctype html>
+<html>
+   <head>
+       <title> Dummy Title </title>
+   </head>
+   <body>
+     <h3> Hello Champ </h3>
+  </body>
+</html>        
+"#;
+
+async fn service_handler(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(INDEX.into())),
+
+        _ => {
+            let response = Response::builder()
+                .status(StatusCode::NOT_FOUND)
+                .body(Body::empty())
+                .unwrap();
+            Ok(response)
+        }
+    }
+    //Ok(Response::new("Micro Service".into()))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = ([127, 0, 0, 1], 8080).into();
 
-    let make_svc = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(hello_rust)) });
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(service_handler)) });
 
     let server = Server::bind(&addr).serve(make_svc);
 
